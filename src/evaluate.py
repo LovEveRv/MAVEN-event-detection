5ff8bb0ee766c58e40fd01b0137ef1055d92f189
LovEver_Rv <yongweili.cn@gmail.com>
2021-05-30 18:31:00 +0800

    fix get_submission

--- src/evaluate.py
+++ src/evaluate.py
@@ -6,32 +6,6 @@ from tqdm import tqdm
 from sklearn.metrics import classification_report
 
 
-def get_submission(args, model, loader):
-    if args.model == 'bert-two-stage':
-        evaluate = evaluate_two_stage_model
-    elif args.model == 'bert-one-stage':
-        evaluate = evaluate_one_stage_model
-    else:
-        raise NotImplementedError()
-    
-    doc_ids, word_ids, pred_list = evaluate(args, model, loader)
-    pred_obj = {}
-    for doc_id, word_id, pred in zip(doc_ids, word_ids, pred_list):
-        if doc_id not in pred_obj:
-            pred_obj[doc_id] = {
-                'id': doc_id,
-                'predictions': []
-            }
-        pred_obj[doc_id]['predictions'].append({
-            'id': word_id,
-            'type_id': pred
-        })
-    jsonl_str = ''
-    for k, v in pred_obj.items():
-        jsonl_str += json.dumps(v) + '\n'
-    return jsonl_str
-
-
 def evaluate_one_stage_model(args, model, loader):
     pass
 
@@ -74,3 +48,53 @@ def evaluate_two_stage_model(args, model, loader):
     print('Average loss_cl: {}'.format(np.mean(loss_cl_list)))
     print(classification_report(gt_labels, pred_list, digits=4))
     return doc_ids, word_ids, pred_list
+
+
+def test(args, model, loader):
+    model.eval()
+
+    print('Testing:')
+    pred_list = []
+    doc_ids   = []
+    word_ids  = []
+    for i, data in tqdm(enumerate(loader)):
+        doc_id, word_id, tokens, _, _ = data
+        doc_ids += doc_id
+        word_ids += word_id
+        if args.cuda:
+            tokens = tokens.cuda()
+        
+        if args.model == 'bert-two-stage':
+            logits_tf, logits_cl = model(tokens)
+            tf_pred = torch.argmax(logits_tf, dim=1).tolist()
+            cl_pred = torch.argmax(logits_cl, dim=1).tolist()
+            for i in range(len(tf_pred)):
+                if tf_pred[i] == 0:
+                    cl_pred[i] = 0
+            pred_list += cl_pred
+        elif args.model == 'bert-one-stage':
+            logits = model(tokens)
+            pred = torch.argmax(logits, dim=1).tolist()
+            pred_list += pred
+        else:
+            return NotImplementedError()
+    return doc_ids, word_ids, pred_list
+
+
+def get_submission(args, model, loader):
+    doc_ids, word_ids, pred_list = test(args, model, loader)
+    pred_obj = {}
+    for doc_id, word_id, pred in zip(doc_ids, word_ids, pred_list):
+        if doc_id not in pred_obj:
+            pred_obj[doc_id] = {
+                'id': doc_id,
+                'predictions': []
+            }
+        pred_obj[doc_id]['predictions'].append({
+            'id': word_id,
+            'type_id': pred
+        })
+    jsonl_str = ''
+    for k, v in pred_obj.items():
+        jsonl_str += json.dumps(v) + '\n'
+    return jsonl_str
